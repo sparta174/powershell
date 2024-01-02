48046054a22eee9947b0b008d7188308a05a618b
ph4lanx <sparta174@gmail.com>
2024-01-02 18:26:00 -0500

    ClearChromeCache.ps1

--- ClearChromeCache.ps1
+++ ClearChromeCache.ps1
@@ -1,5 +1,5 @@
 #Get Computer List
-$ComputerList = Import-CSV "C:\Users\nross\Documents\PCList1.csv" | Select-Object -ExpandProperty Name
+$ComputerList = Import-CSV "C:\Users\*\Documents\PCList1.csv" | Select-Object -ExpandProperty Name
 
 #Clear the Chrome cache
 foreach ($ComputerName in $ComputerList) {
