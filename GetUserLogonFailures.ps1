fe7768ad66c4dfb1f01ba5af63898ce54d20d87c
ph4lanx <sparta174@gmail.com>
2024-01-02 19:10:00 -0500

    Update GetUserLogonFailures.ps1

--- GetUserLogonFailures.ps1
+++ GetUserLogonFailures.ps1
@@ -7,4 +7,4 @@ foreach($D in $DC) {
 }
 
 
-Get-EventLog -LogName Security -After (Get-Date).AddDays(-1) -ComputerName HS1DCHQ01 -Message *carimbocasi* | Where { $_.EventID -eq 4625 }
+Get-EventLog -LogName Security -After (Get-Date).AddDays(-1) -ComputerName HS1DCHQ01 -Message ** | Where { $_.EventID -eq 4625 }
