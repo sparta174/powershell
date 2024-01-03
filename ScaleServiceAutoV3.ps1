0abade057d3b9a69397b99b78c958d94e45611e2
ph4lanx <sparta174@gmail.com>
2024-01-02 19:16:08 -0500

    Delete ScaleServiceAutoV3.ps1

--- ScaleServiceAutoV3.ps1
+++
@@ -1,6 +0,0 @@
-# Get Computer List
-$ComputerList = Import-CSV "\\B79FS\Apps\ITDocs\Powershell Scripts\TestList4.csv" | Select-Object -ExpandProperty Name
-
-foreach($Computer in $ComputerList){
-Invoke-Command -FilePath '\\B79FS\Apps\ITDOCS\Powershell Scripts\TestScale.ps1' -ComputerName $Computer
-}
