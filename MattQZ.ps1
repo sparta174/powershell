8d1d1fc0d9d8d54bbf71277115c360d189f6bf60
ph4lanx <sparta174@gmail.com>
2024-01-02 19:12:27 -0500

    Update MattQZ.ps1

--- MattQZ.ps1
+++ MattQZ.ps1
@@ -1,1 +1,1 @@
-﻿Invoke-Command -Computername B79MPC20 -Scriptblock { taskkill /F /IM javaw.exe; 'C:\Program Files\QZ Tray\qz-tray.exe' }
+﻿Invoke-Command -Computername * -Scriptblock { taskkill /F /IM javaw.exe; 'C:\Program Files\QZ Tray\qz-tray.exe' }
